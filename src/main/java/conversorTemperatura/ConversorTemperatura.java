888bc41ce977b209d9d7c5573e03a0a4be2ff673
Vitoria <2411550024@fema.edu.br>
2025-06-10 20:44:25 -0300

    Feat: Criar um programa para converter temperatura

---
+++ src/main/java/conversorTemperatura/ConversorTemperatura.java
@@ -0,0 +1,12 @@
+package conversorTemperatura;
+
+public class ConversorTemperatura {
+    public static double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitParaCelsius(double fahrenheit) {
+        return 5.0 / 9 * (fahrenheit - 32);
+    }
+}
+
