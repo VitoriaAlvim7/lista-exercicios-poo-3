630932c9a3baf553530f753855cd17c72714fd5d
Vitoria <2411550024@fema.edu.br>
2025-06-10 20:42:43 -0300

    Feat: Criar um programa para lancamento de dardos

---
+++ src/main/java/lancadorDardo/LancarDardo.java
@@ -0,0 +1,25 @@
+package lancadorDardo;
+
+public class LancarDardo {
+    private double dardo1;
+    private double dardo2;
+    private double dardo3;
+
+    public LancarDardo(double dardo1, double dardo2, double dardo3) {
+        this.dardo1 = dardo1;
+        this.dardo2 = dardo2;
+        this.dardo3 = dardo3;
+    }
+
+    public double maiorDistancia() {
+        double maior = dardo1;
+        if (dardo2 > maior) {
+            maior = dardo2;
+        }
+        if (dardo3 > maior) {
+            maior = dardo3;
+        }
+        return maior;
+    }
+
+}
