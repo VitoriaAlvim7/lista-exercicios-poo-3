b38914abc18b42d3f2b4981e254fab6546a72fa8
Vitoria <2411550024@fema.edu.br>
2025-06-10 20:13:28 -0300

    Feat: Criar um programa para retornar o valor do troco ou falta de uma compra

---
+++ src/main/java/trocoFalta/TrocoOuFalta.java
@@ -0,0 +1,18 @@
+package trocoFalta;
+
+public class TrocoOuFalta {
+
+    private float valorUnitario;
+    private int quantidadeComprada;
+    private float valorPago;
+
+    public TrocoOuFalta(float valorUnitario, int quantidadeComprada, float valorPago) {
+        this.valorUnitario = valorUnitario;
+        this.quantidadeComprada = quantidadeComprada;
+        this.valorPago = valorPago;
+    }
+
+    public float calcularTotalCompra() {
+        return valorUnitario * quantidadeComprada;
+    }
+}
